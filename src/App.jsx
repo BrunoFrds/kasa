e1453b9044c5a2bb7f98d482d8cc7598ce5b2268
BrunoFrds <brunofrdsdev@gmail.com>
2023-10-10 11:04:17 +0200

    Ajout des fichiers Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Accueil from "./Pages/Accueil";
+import APropos from "./Pages/APropos";
+import Fiche from "./Pages/Fiche";
+import Error from "./Pages/404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+        <Route path="/apropos" element={<APropos />} />
+        <Route path="/fiche/:id" element={<Fiche />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
