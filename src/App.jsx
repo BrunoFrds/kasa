630680fb61931311a309892f4d02c3328159b9de
BrunoFrds <brunofrdsdev@gmail.com>
2023-10-10 11:09:39 +0200

    Ajout des fichiers Layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Layouts/Header";
+import Footer from "./Layouts/Footer";
 import Accueil from "./Pages/Accueil";
 import APropos from "./Pages/APropos";
 import Fiche from "./Pages/Fiche";
@@ -8,12 +10,14 @@ import Error from "./Pages/404";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Accueil />} />
         <Route path="/apropos" element={<APropos />} />
         <Route path="/fiche/:id" element={<Fiche />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
